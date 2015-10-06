b76fc6c4ae0efa2cad9e7d4d49e679811c070451
Veronica Rivera <damediabolique@gmail.com>
2015-10-06 11:39:51 -0400

    "Added Slide Out Menu and began writing pages"

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 console.log("Hello");
+
+$(function(){												
+       
+       //Navigation Menu Slider
+        $('#nav-expander').on('click',function(e){
+      		e.preventDefault();
+      		$('body').toggleClass('nav-expanded');
+      		$('#nav-expander').addClass('hidden');
+      	});
+      	$('#nav-close').on('click',function(e){
+      		e.preventDefault();
+      		$('body').removeClass('nav-expanded');
+      		$('#nav-expander').removeClass('hidden');
+      	});
+        	
+});
