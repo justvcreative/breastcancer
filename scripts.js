676f092d8f0a5443e8847be8f0286fb12c5b84a8
Veronica Rivera <damediabolique@gmail.com>
2015-10-06 20:48:55 -0400

    Added Profile Image and Modified HTML, CSS, JS files

--- scripts.js
+++ scripts.js
@@ -6,10 +6,14 @@ $(function(){
         $('#nav-expander').on('click',function(e){
       		e.preventDefault();
       		$('body').toggleClass('nav-expanded');
+      		$('#nav-expander').hide();
+      		$('#donate-icon').hide();
       	});
       	$('#nav-close').on('click',function(e){
       		e.preventDefault();
       		$('body').removeClass('nav-expanded');
+      		$('#nav-expander').show();
+      		$('#donate-icon').show();
       	});
         	
 });
